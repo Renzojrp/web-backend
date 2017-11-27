9c1aee04510a5d6d7533a92597467456b2c65179
Renzojrp <1410327@utp.edu.pe>
2017-11-27 01:22:32 -0500

    Contract

--- controllers/contract.js
+++ controllers/contract.js
@@ -99,20 +99,20 @@ function  getContractByMusician (req, res){
 function getContractByCraftman (req, res){
   let craftmanId = req.params.craftmanId
 
-  Contract.find({"musician":musicianId}, (err, contracts) => {
+  Contract.find({"craftman":craftmanId}, (err, contracts) => {
     if(err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
     if(!contracts) return res.status(484).send({message: `No existen contratos para este artesano: ${contracId}`})
 
-    Publication.populate(contract, {path: "publication"}, function(err, contract){
-      Instrument.populate(contract, {path: "publicaton.instrument"}, function(err, contract){
-        Musician.populate(contract, {path: "publicaton.instrument.musician"}, function(err, contract){
-          Musician.populate(contract, {path: "musician"}, function(err, contract){
-            User.populate(contract, {path: "publicaton.instrument.musician.user"}, function(err, contract){
-              Craftman.populate(contract, {path: "craftman"}, function(err, contract){
-                User.populate(contract, {path: "musician.user"}, function(err, contract){
-                  User.populate(contract, {path: "craftman.user"}, function(err, contract){
-                    Order.populate(contract, {path: "order"}, function(err, contract){
-                      res.send(200, { contract })
+    Publication.populate(contracts, {path: "publication"}, function(err, contracts){
+      Instrument.populate(contracts, {path: "publicaton.instrument"}, function(err, contracts){
+        Musician.populate(contracts, {path: "publicaton.instrument.musician"}, function(err, contracts){
+          Musician.populate(contracts, {path: "musician"}, function(err, contracts){
+            User.populate(contracts, {path: "publicaton.instrument.musician.user"}, function(err, contracts){
+              Craftman.populate(contracts, {path: "craftman"}, function(err, contracts){
+                User.populate(contracts, {path: "musician.user"}, function(err, contracts){
+                  User.populate(contracts, {path: "craftman.user"}, function(err, contracts){
+                    Order.populate(contracts, {path: "order"}, function(err, contracts){
+                      res.send(200, { contracts })
                     });
                   });
                 });
