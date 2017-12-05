13b4f98f0f5e6ef9c00ff070ec3db50e97e08b5e
Renzojrp <1410327@utp.edu.pe>
2017-12-05 16:36:51 -0500

    Contract

--- models/contract.js
+++ models/contract.js
@@ -2,20 +2,19 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const Publication = mongoose.model('Publication')
-const Order = mongoose.model('Order')
+const Instrument = mongoose.model('Instrument')
 const Musician = mongoose.model('Musician')
 const Craftman = mongoose.model('Craftman')
 
 const ContractSchema = new Schema({
-  publication: {type: Schema.ObjectId, ref: "Publication"},
-  order: {type: Schema.ObjectId, ref: "Order"},
   musician: {type: Schema.ObjectId, ref: "Musician"},
   craftman: {type: Schema.ObjectId, ref: "Craftman"},
   price: Number,
   date: { type: Date, default: Date.now() },
   status: { type: String, default: "A" },
   type: String,
+  instrument: {type: Schema.ObjectId, ref: "Instrument"},
+  description: String,
 })
 
 module.exports = mongoose.model('Contract', ContractSchema)
